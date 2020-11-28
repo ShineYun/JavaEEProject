0e3351a6e55dcd8185aee69da130752c936ddaa9
ShineYun <2568922497@qq.com>
2020-11-28 20:05:43 +0800

    md5加密

---
+++ src/main/java/cn/Kenelm/util/MD5.java
@@ -0,0 +1,29 @@
+package cn.Kenelm.util;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public abstract class MD5 {
+    static {
+        try {
+            MessageDigest.getInstance("MD5"); // confirm that the algorithm exists
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+
+    public static String md5(String s) {
+        try {
+            final MessageDigest digester = MessageDigest.getInstance("MD5");
+            final byte[] digest = digester.digest(s.getBytes(StandardCharsets.UTF_8));
+            final StringBuilder builder = new StringBuilder();
+            for (byte b : digest)
+                builder.append(Integer.toHexString((b & 0xFF) | 0x100), 1, 3);
+            return builder.toString();
+        } catch (NoSuchAlgorithmException e) {
+            return null;
+        }
+    }
+}
